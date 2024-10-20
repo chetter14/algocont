05ccf5e03c572b14fa3ed791d9e226b803e379ed
Artem Leshchukov <artemleshchukov02@gmail.com>
2024-10-20 17:05:48 +0300

    Formatted the 0 lab to fit clang format

--- contest/lab0/main.cpp
+++ contest/lab0/main.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include <string>
 
+int main() {
+  int input_strings_number = 0;
+  std::cin >> input_strings_number;
 
-int main()
-{
-    std::cout << "Hello World from lab0!\n";
-    return 0;
+  std::string temp;
+  for (int i = 0; i < input_strings_number; ++i) {
+    std::cin >> temp;
+    if (temp.size() % 2 != 0) {
+      std::cout << "NO\n";
+      continue;
+    }
+
+    size_t left = 0;
+    for (size_t right = temp.size() / 2; left < temp.size() / 2; ++left, ++right) {
+      if (temp[left] != temp[right]) {
+        std::cout << "NO\n";
+        break;
+      }
+    }
+
+    if (left == temp.size() / 2) {
+      std::cout << "YES\n";
+    }
+  }
+
+  return 0;
 }
