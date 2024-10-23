2b0c30f2d05e813506278cd8360596f3ccc47436
Artem Leshchukov <artemleshchukov02@gmail.com>
2024-10-23 21:22:49 +0300

    Wrote algorithm steps to complete the 2nd task

---
+++ contest/lab2/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+// Data Structure: an array of bits of 1'000'000 size (10^6 bits), 
+//  implement via std::bitset and count variable
+
+// Functionality:
+// 1) + and - operations
+//  do math on bitset and in the process of calculations increment/decrement 
+//  {count} variable
+
+
+int main() {
+  
+  return 0;
+}
